96bc58899515808f44d440b8281f7b283d522337
HS01 <sonicman614@gmail.com>
2023-09-12 11:37:54 -0400

    Removed Candy.java and added a text fie

--- TrickorTreat.java
+++ TrickorTreat.java
@@ -6,9 +6,10 @@ public class TrickorTreat {
         //Test run for the candy class
 
         for (int i = 0; i < 10; i++){
-            Candy test = new Candy();
-            System.out.println(test);
-
+           //Candy test = new Candy();
+            //System.out.println(test);
+            House testHouse = new House();
+            System.out.println(testHouse);
         }
           
             
